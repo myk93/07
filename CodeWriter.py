710f14ab86c00ae8c1a7c932f4d9d51640e9f3bb
myk93 <meni.kroizer@gmail.com>
2021-11-01 12:47:49 +0200

    working vrsn

---
+++ CodeWriter.py
@@ -0,0 +1,133 @@
+"""This file is part of nand2tetris, as taught in The Hebrew University,
+and was written by Aviv Yaish according to the specifications given in  
+https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
+and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
+Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+
+
+class CodeWriter:
+    """Translates VM commands into Hack assembly code."""
+
+    def __init__(self, output_stream: typing.TextIO) -> None:
+        """Initializes the CodeWriter.
+
+        Args:
+            output_stream (typing.TextIO): output stream.
+        """
+        # Your code goes here!
+        self.out_put = output_stream
+        self.index = 0
+        pass
+
+    def set_file_name(self, filename: str) -> None:
+        """Informs the code writer that the translation of a new VM file is 
+        started.
+
+        Args:
+            filename (str): The name of the VM file.
+        """
+        # Your code goes here!
+        # self.out_put. = filename
+        pass
+
+    def write_arithmetic(self, command: str) -> None:
+        """Writes the assembly code that is the translation of the given 
+        arithmetic command.
+
+        Args:
+            command (str): an arithmetic command.
+        """
+        # Your code goes here!
+        translated_command = ""
+        translated_command = self.sp_pop()  # POP X
+        if command == "neg":
+            translated_command += "D=-D\n"
+        elif command == "not":
+            translated_command += "D=!D\n"
+        else:
+            translated_command += "@SP\nAM=M-1\n"  # POP Y
+            if command == "add":
+                translated_command += "D=D+M\n"  # ADD
+            elif command == "sub":
+                translated_command += "D=M-D\n"  # sub
+            elif command == "and":
+                translated_command += "D=D&M\n"  # AND
+            elif command == "or":
+                translated_command += "D=D|M\n"  # OR
+            else:
+                if command == "eq":
+                    jump_type ="JEQ"
+                elif command == "gt":
+                    jump_type ="JGT"
+                else: #command == "lt":
+                    jump_type ="JLT"
+                translated_command += "D=M-D\n" + "@TRUE" + str(self.index) + "\n" + "D;"+jump_type+"\n" + "D=0\n" + "@CONTINUE" + str(self.index) + "\n" + "0;JMP\n" +"(TRUE" + str(self.index) + ")\n" +"D=-1\n" +"(CONTINUE" + str(self.index) + ")\n"
+                self.index += 1
+        translated_command += "@SP\n" + "A=M\n" + "M=D\n"
+        translated_command += "@SP\nM=M+1\n"  # sp = sp+1
+        self.out_put.write(translated_command)
+
+    def write_push_pop(self, command: str, segment: str, index: int) -> None:
+        """Writes the assembly code that is the translation of the given 
+        command, where command is either C_PUSH or C_POP.
+
+        Args:
+            command (str): "C_PUSH" or "C_POP".
+            segment (str): the memory segment to operate on.
+            index (int): the index in the memory segment.
+        """
+        # Your code goes here!
+        translated_code = ""
+        if segment == "local":
+            start = "LCL"
+        elif segment == "argument":
+            start = "ARG"
+        elif segment == "this":
+            start = "THIS"
+        elif segment == "that":
+            start = "THAT"
+        else:
+            start = None
+            if segment == "pointer":
+                if index == 0:
+                    temp = "@THIS"
+                else:
+                    temp = "@THAT"
+                if command == "C_PUSH":
+                    translated_code = temp + "\n" + "D=M\n" + self.sp_push()#here
+                else:
+                    translated_code = self.sp_pop() + temp + "\n" +"M=D\n"
+            elif segment == "constant":
+                translated_code = "@" + str(index) + "\n" + "D=A\n" + self.sp_push()#CHECK
+            elif segment == "temp":
+                if command == "C_PUSH":
+                    translated_code += "@5\nD=A\n@{0}\nA=D+A\nD=M\n".format(str(index)) + self.sp_push() # CHECK here
+                else:  # POP
+                    translated_code += "@5\nD=A\n@{0}\nD=D+A\n@adder\nM=D\n{1}@adder\nA=M\nM=D\n".format(
+                        str(index),self.sp_pop())  # CHECK
+            else:  # segment == STATIC
+                if command == "C_PUSH":
+                    translated_code += "@f.{0}\nD=M\n@SP\n".format(str(index)) + self.sp_push()#CHECK
+                else:
+                    translated_code += "{1}@f.{0}\nM=D\n".format(str(index),self.sp_pop())#CHECK
+        if start is not None:
+            translated_code += "@{1}\nD=M\n@{0}\nA=D+A\n".format(str(index), start)
+            if command == "C_PUSH":
+                translated_code += "D=M\n" + self.sp_push()  # CHECK
+            else:  # POP
+                translated_code += "@adder\nM=D\n{0}@adder\nA=M\nM=D\n".format(self.sp_pop())  # CHECK
+        self.out_put.write(translated_code)
+
+    def close(self) -> None:
+        """Closes the output file."""
+        # Your code goes here!
+        self.out_put.close()
+        pass
+
+    def sp_push(self):#pushing to stack
+        return "@SP\n" + "A=M\n" + "M=D\n" + "@SP\n"+"M=M+1\n"
+
+    def sp_pop(self):#poping from stack
+        return "@SP\n"+"AM=M-1\n"+"D=M\n"
---
+++ Parser.py
@@ -0,0 +1,100 @@
+"""This file is part of nand2tetris, as taught in The Hebrew University,
+and was written by Aviv Yaish according to the specifications given in  
+https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
+and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
+Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+
+
+class Parser:
+    """
+    Handles the parsing of a single .vm file, and encapsulates access to the
+    input code. It reads VM commands, parses them, and provides convenient 
+    access to their components. 
+    In addition, it removes all white space and comments.
+    """
+
+    def __init__(self, input_file: typing.TextIO) -> None:
+        """Gets ready to parse the input file.
+
+        Args:
+            input_file (typing.TextIO): input file.
+        """
+        # Your code goes here!
+        # A good place to start is:
+        self.input_lines = input_file.read().splitlines()
+        self.remove_comments()
+        self.current_line = -1
+
+    def has_more_commands(self) -> bool:
+        """Are there more commands in the input?
+
+        Returns:
+            bool: True if there are more commands, False otherwise.
+        """
+        # Your code goes here!
+        if len(self.input_lines) == self.current_line:
+            return False
+        return True
+
+    def advance(self) -> None:
+        """Reads the next command from the input and makes it the current 
+        command. Should be called only if has_more_commands() is true. Initially
+        there is no current command.
+        """
+        # Your code goes here!
+        if self.has_more_commands():
+            self.current_line += 1
+
+    def command_type(self) -> str:
+        """
+        Returns:
+            str: the type of the current VM command.
+            "C_ARITHMETIC" is returned for all arithmetic commands.
+            For other commands, can return:
+            "C_PUSH", "C_POP", "C_LABEL", "C_GOTO", "C_IF", "C_FUNCTION",
+            "C_RETURN", "C_CALL".
+        """
+        # Your code goes here!
+        if self.input_lines[self.current_line].split(" ")[0] == "pop":
+            return "C_POP"
+        elif self.input_lines[self.current_line].split(" ")[0] == "push":
+            return "C_PUSH"
+        else:
+            return "C_ARITHMETIC"
+        pass
+
+    def arg1(self) -> str:
+        """
+        Returns:
+            str: the first argument of the current command. In case of 
+            "C_ARITHMETIC", the command itself (add, sub, etc.) is returned. 
+            Should not be called if the current command is "C_RETURN".
+        """
+        # Your code goes here!
+        if self.command_type() == "C_ARITHMETIC":
+            return self.input_lines[self.current_line].split("//")[0].replace(" ", "")
+        else:
+            return self.input_lines[self.current_line].split(" ")[1]
+        pass
+
+    def arg2(self) -> int:
+        """
+        Returns:
+            int: the second argument of the current command. Should be
+            called only if the current command is "C_PUSH", "C_POP", 
+            "C_FUNCTION" or "C_CALL".
+        """
+        # Your code goes here!
+        return int(self.input_lines[self.current_line].split(" ")[2])
+        pass
+
+    def remove_comments(self):
+        i = 0
+        while i != len(self.input_lines):
+            if len(self.input_lines[i].replace(" ", "")) == 0 or self.input_lines[i].replace(" ", "")[0] == "/":
+                self.input_lines.pop(i)
+            else:
+                ' '.join(self.input_lines[i].split())
+                i += 1
